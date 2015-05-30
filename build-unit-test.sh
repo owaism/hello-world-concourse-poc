c2f8774b8222cde20d248236b2533b7e8b846603
Owais Mohamed <equa.monde@gmail.com>
2015-05-30 13:14:11 -0400

    Changes to the script

--- build-unit-test.sh
+++ build-unit-test.sh
@@ -4,6 +4,6 @@ echo "Java Version: $(java -version)"
 ls
 echo "TERM: $TERM"
 export GRADLE_OPTS=-Dorg.gradle.native=false
-gradle -b $1 war
+gradle -b $1/build.gradle war
 version=$(cat $3)
 mv $1/build/libs/*.war ./$2-$version.war
