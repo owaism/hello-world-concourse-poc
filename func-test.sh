506b2a3c7bc54a0891f66961fb2224816c9874d9
Owais Mohamed <equa.monde@gmail.com>
2015-06-02 14:06:06 -0400

    Changes to the script

--- func-test.sh
+++ func-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export GRADLE_OPTS=-Dorg.gradle.native=false
+sudo export GRADLE_OPTS=-Dorg.gradle.native=false
 echo "Starting func-test"
 gradle -b $1/build.gradle ftesting -Phttps://hello-world-concourse.run.covisintrnd.com
