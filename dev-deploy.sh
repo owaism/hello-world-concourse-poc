fcbfa34eb2888bf6da9e16c42c23e82eea9197a0
Owais Mohamed <equa.monde@gmail.com>
2015-06-02 15:25:35 -0400

    Changed permissions on script

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -2,4 +2,4 @@
 mv $1/*.war ./deployable-artifact.war
 mv $2/*.yml ./manifest.yml
 version=$(cat $3/number)
-cp ./manifest.yml hello-world-app-dev-rc-$version.yml
+cp ./manifest.yml hello-world-app-dev-$version.yml
