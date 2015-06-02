2348bdc577c0d7c11286c5dbb48d5183dd0e9eed
Owais Mohamed <equa.monde@gmail.com>
2015-06-02 14:11:23 -0400

    Changed permissions on script

--- func-test.sh
+++ func-test.sh
@@ -2,4 +2,4 @@
 echo "Setting gradle opts"
 export GRADLE_OPTS=-Dorg.gradle.native=false
 echo "Starting func-test"
-gradle -b $1/build.gradle ftesting -Phttps://hello-world-concourse.run.covisintrnd.com
+gradle -b $1/build.gradle $2 $3
